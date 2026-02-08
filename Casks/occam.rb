695855c55cd79bf792d3f4e46b15a07c2b9610eb
Michal Palczewski <michalp@nimbusdev.io>
2026-02-08 08:19:42 -0600

    Fix version to semver 1.0.0 and update cask SHA256

--- Casks/occam.rb
+++ Casks/occam.rb
@@ -1,6 +1,6 @@
 cask "occam" do
   version "1.0.0"
-  sha256 "PLACEHOLDER_SHA256"
+  sha256 "9c4be910c03edc1ed29e5942ca62aa357fc1519c5231bbecc4a6d01966c65531"
 
   url "https://github.com/mpalczew/occam/releases/download/v#{version}/Occam-#{version}-universal.zip"
   name "Occam"
