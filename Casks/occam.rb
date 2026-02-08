ca347cc0721e6163a22ccc1c5eb9246e0c9ab65b
Michal Palczewski <michalp@nimbusdev.io>
2026-02-08 11:03:30 -0600

    Update cask SHA256 for signed v1.2.0 artifact

--- Casks/occam.rb
+++ Casks/occam.rb
@@ -1,6 +1,6 @@
 cask "occam" do
   version "1.2.0"
-  sha256 "a182021808134e951dee53150fd32b44b9878e844d6e29728bd3c4b0d7ebd201"
+  sha256 "7f0011010ecff4b73da6c4f09845648539aee6f1d25a2295862e96eba592b123"
 
   url "https://github.com/mpalczew/occam/releases/download/v#{version}/Occam-#{version}-universal.zip"
   name "Occam"
